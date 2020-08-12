2f5378b54716b167ebd9fb5402f5aead0171d74a
Jugpreet Kaur <37807605+jugpreet@users.noreply.github.com>
2020-08-12 16:45:35 +0530

    src

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReadData from './ReadData';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ReadData/>
+    </div>
+  );
+}
+
+export default App;
